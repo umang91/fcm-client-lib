efdf5b3e5792bd152ed2b1341b53ad356e5fd121
umang91 <umangchamaria@gmail.com>
2022-01-14 19:37:02 +0530

    agp update, example gradle moved to kotlin

---
+++ example/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.umang.example.fcmclient"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation(libs.kotlin.stdLib)
+    implementation(project(":fcm-client"))
+}
+apply(plugin= "com.google.gms.google-services")
